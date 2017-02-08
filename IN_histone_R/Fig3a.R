b273a0354e9648d59889a66af0094954a3c72ff6
saralinker <sara.linker@gmail.com>
2017-02-08 09:29:20 -0800

    first commit

---
+++ IN_histone_R/Fig3a.R
@@ -0,0 +1,5 @@
+#############
+# Inigo's Histone Analysis
+# Figure 3a
+
+dat <- read.table("~/Documents/SalkProjects/Inigo/IN_histone/IN_histone_raw/RNAseq.CvsH.hg19.analyzeRepats.txt",header = TRUE,skip = 1)
